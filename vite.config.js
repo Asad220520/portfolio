191d70bec7619ea1c53bd9443af1fbaf7b17950e
Asadhkm <asadhkm4@gmail.com>
2025-08-06 18:11:22 +0300

    Fix Vercel config and paths

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./", // 👈 важно для корректной работы в Vercel
+  build: {
+    outDir: "dist", // важно!
+  },
 });
